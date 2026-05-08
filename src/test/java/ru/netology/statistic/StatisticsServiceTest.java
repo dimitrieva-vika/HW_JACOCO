572c9a5af8a57ae351596d252dbd4785a3217e7c
виктория димитриева <erschova.tamarah@yandex.ru>
2026-05-08 13:05:18 +1000

    100% branch coverage achieved - all tests passing

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.Assertions;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxStandard() {
         StatisticsService service = new StatisticsService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-        long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomes);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void findMaxWhenFirstIsMax() {
+    void findMaxFirstElement() {
         StatisticsService service = new StatisticsService();
         long[] incomes = {100, 1, 2, 3, 4};
         long expected = 100;
@@ -24,7 +24,7 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findMaxWhenLastIsMax() {
+    void findMaxLastElement() {
         StatisticsService service = new StatisticsService();
         long[] incomes = {1, 2, 3, 4, 100};
         long expected = 100;
@@ -33,7 +33,7 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findMaxWhenAllEqual() {
+    void findMaxAllEqual() {
         StatisticsService service = new StatisticsService();
         long[] incomes = {5, 5, 5, 5, 5};
         long expected = 5;
@@ -42,7 +42,7 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findMaxWhenOnlyOneElement() {
+    void findMaxSingleElement() {
         StatisticsService service = new StatisticsService();
         long[] incomes = {42};
         long expected = 42;
@@ -51,9 +51,18 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findMaxWhenMaxInMiddle() {
+    void findMaxMiddleElement() {
         StatisticsService service = new StatisticsService();
-        long[] incomes = {10, 20, 50, 30, 40};
+        long[] incomes = {1, 3, 7, 2, 0};
+        long expected = 7;
+        long actual = service.findMax(incomes);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxRepeatedValue() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {50, 30, 50, 20, 50};
         long expected = 50;
         long actual = service.findMax(incomes);
         Assertions.assertEquals(expected, actual);
